cf89a7d3a42f5b50a02cbbd0f7c990b68cdff63c
Shelpec <se400045@gmail.com>
2024-02-08 22:52:55 +0600

    READ_ME база даных

--- src/main/java/ru/kors/springstudents/service/impl/StudentServiceImpl.java
+++ src/main/java/ru/kors/springstudents/service/impl/StudentServiceImpl.java
@@ -14,6 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 @Primary
 public class StudentServiceImpl implements StudentService {
+
     private final StudentRepository repository;
     @Override
     public List<Student> findAllStudent() {
