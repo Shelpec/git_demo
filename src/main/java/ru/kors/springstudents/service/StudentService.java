61f3b1e309e684564e9f54668d3f8e96b3bf4206
Shelpec <se400045@gmail.com>
2024-02-08 22:27:33 +0600

    video commit

---
+++ src/main/java/ru/kors/springstudents/service/StudentService.java
@@ -0,0 +1,15 @@
+package ru.kors.springstudents.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.kors.springstudents.model.Student;
+
+import java.util.List;
+
+public interface StudentService {
+    public List<Student> findAllStudent();
+    Student saveStudent(Student student);
+    Student findByEmail(String email);
+    Student updateStudent(Student student);
+    void deleteStudent(String email);
+}
